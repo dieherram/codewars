be1b16a228e921cdabb910e2e4b17d96324dfe1b
Diego Hernández <dieherram@gmail.com>
2024-02-12 13:47:19 -0300

    actualizacion archivo script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,19 @@ Note! The input is a Real number (actual type is language dependent) and is >= 0
 function cockroachSpeed(s) {
     return cmPerSec = Math.floor(s * 27.778)
   }
+
+/*
+Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+*/
+
+//Solution
+function findAverage(array) {
+    if (array.length > 0) {
+      const average = array.reduce((acc, currVal) => {
+        return acc + currVal
+      }, 0)
+      return average / array.length;
+    } else { return 0 }
+  }

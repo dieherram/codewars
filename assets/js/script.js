2b9e4693b724155c928830dbf59ebff021f4ef2b
Diego Hernández <dieherram@gmail.com>
2024-02-28 20:09:39 -0300

    update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,27 @@ function findAverage(array) {
       return average / array.length;
     } else { return 0 }
   }
+
+/*
+Given an array of integers your solution should find the smallest integer.
+
+  For example:
+  
+      Given [34, 15, 88, 2] your solution will return 2
+      Given [34, -345, -1, 100] your solution will return -345
+  
+  You can assume, for the purpose of this kata, that the supplied array will not be empty.
+  */
+
+  //Solution
+  class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      if (args.length > 0) {
+        args.sort((a, b) => {
+          return a - b
+        })
+        return args[0]
+      }
+    }
+  }
+  

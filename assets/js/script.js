8c30f89b4028dd867c7570f64efaad32b1ec1790
Diego Hernández <dieherram@gmail.com>
2024-03-01 01:00:47 -0300

    update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,3 +89,15 @@ And a tower with 6 floors looks like this:
   }
   return tower
 }
+
+/* Write a function to split a string and convert it into an array of words.
+Examples (Input ==> Output):
+
+"Robin Singh" ==> ["Robin", "Singh"]
+
+"I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+ */
+
+ //Solution
+ const stringToArray = (string) => string.split(' ')

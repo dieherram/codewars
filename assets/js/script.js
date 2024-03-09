c7ae5a2d91e1806830e56fdb232160d4efca79df
Diego Hernández <dieherram@gmail.com>
2024-03-09 17:56:41 -0300

    update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -113,3 +113,17 @@ For example:
 function maps(x) {
   return x.map(el => el * 2)
 }
+
+/* Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+
+You can assume that all values are integers. Do not mutate the input array/list.
+ */
+
+//Solution
+function invert(array) {
+  return array.length > 0 ? array.map(el => el - (el * 2)) : []
+}

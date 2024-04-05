dccab142eecd8271539e0664e8eab11d3a7e1316
Diego Hernández <dieherram@gmail.com>
2024-04-05 14:57:31 -0300

    update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -181,3 +181,16 @@ function shortcut(string) {
 
   return stringFiltered;
 }
+
+/*
+
+Write a function that returns a string in which firstname is swapped with last name.
+
+Example(Input --> Output)
+
+"john McClane" --> "McClane john"
+
+*/
+
+//Solution
+const nameShuffler = (str) => str.split(' ').reverse().join(' ')

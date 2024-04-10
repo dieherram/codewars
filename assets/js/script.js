243c13b5fee81eac335e1dbbe17e0ce5a400b4c9
Diego Hernández <dieherram@gmail.com>
2024-04-10 19:48:27 -0400

    update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -232,3 +232,24 @@ Now you have to write a function that takes an argument and returns the square o
 
 //Solution
 const square = (num) => num * num
+
+/*
+
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+
+Names given are always valid strings.
+
+
+*/
+
+//Solution
+const areYouPlayingBanjo = (name) => {
+  const nameArray = name.split('')
+  return nameArray[0].toLowerCase() === 'r' ? `${name} plays banjo` : `${name} does not play banjo`
+}

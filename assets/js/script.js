5faefe02750cfa0723525e22793e1bbba46cd859
Diego Hernández <dieherram@gmail.com>
2024-04-12 16:38:39 -0400

    update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -245,11 +245,37 @@ name + " does not play banjo"
 
 Names given are always valid strings.
 
-
 */
 
 //Solution
 const areYouPlayingBanjo = (name) => {
   const nameArray = name.split('')
   return nameArray[0].toLowerCase() === 'r' ? `${name} plays banjo` : `${name} does not play banjo`
+}
+
+/*
+
+Issue
+
+Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+
+The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+The pipes are correct when each pipe after the first is 1 more than the previous one.
+Task
+
+Given a list of unique numbers sorted in ascending order, return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+Example
+
+Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+
+*/
+
+//Solution
+const pipeFix = (numbers) => {
+  let numbersArray = []
+  for (let i = numbers[0]; i <= numbers[numbers.length - 1]; i++) {
+    numbersArray.push(i)
+  }
+  return numbersArray
 }
